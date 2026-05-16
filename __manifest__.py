7ca503d42aead23b25b7790f635d933cfca162cf
Robert-Jan Spliethoff <robert-jan@spliethoff.net>
2026-05-16 11:22:13 +0200

    Revert "Reduce horizontal padding to widen receipt"
    This reverts commit 14afb7e7e501e169a60c859c2c5378f8496ae4ed.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sailspecials POS Receipt',
-    'version': '17.0.1.1.2',
+    'version': '17.0.1.1.1',
     'summary': 'Custom POS receipt layout for Sailspecials',
     'category': 'Point of Sale',
     'depends': ['point_of_sale'],
